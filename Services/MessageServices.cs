e8ef7960dcbeae2d0609f8889e2ddb73d870a3a7
Bharati S <bhartishinde01@gmail.com>
2017-08-22 16:19:32 +0530

    Attachment with event

--- Services/MessageServices.cs
+++ Services/MessageServices.cs
@@ -99,8 +99,6 @@ namespace ESPL.MailService.Services
 
                 m.Importance = MessageImportance.Normal;
 
-                BodyBuilder bodyBuilder = new BodyBuilder();
-
                 //create ics file for event
                 System.Text.StringBuilder sb = new System.Text.StringBuilder();
                 string DateFormat = "yyyyMMddTHHmmssZ";
@@ -130,8 +128,29 @@ namespace ESPL.MailService.Services
 
                 var calendarBytes = System.Text.Encoding.UTF8.GetBytes(sb.ToString());
                 System.IO.MemoryStream ms = new System.IO.MemoryStream(calendarBytes);
+
+                BodyBuilder bodyBuilder = new BodyBuilder();
                 bodyBuilder.Attachments.Add("event.ics", ms);
-                m.Body = bodyBuilder.ToMessageBody();
+                //m.Body = bodyBuilder.ToMessageBody();
+
+                var multipart = new Multipart("mixed");
+                multipart.Add(bodyBuilder.ToMessageBody());
+
+                if (eventOptions.attachment != null)
+                {
+                    byte[] newBytes = eventOptions.attachment;
+                    MemoryStream msAttachment = new MemoryStream(newBytes);
+                    var attachment = new MimePart("image", "gif")
+                    {
+                        ContentObject = new ContentObject(msAttachment),
+                        ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                        ContentTransferEncoding = ContentEncoding.Base64,
+                        FileName = eventOptions.attachmentName
+                    };
+                    multipart.Add(attachment);
+                }
+
+                m.Body = multipart;
 
                 return m;
             }
